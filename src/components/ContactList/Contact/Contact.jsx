76efc38ad8e557d7a5c2511e6172a27a066a5c9f
goodcat46 <107672708+goodcat46@users.noreply.github.com>
2022-10-29 01:14:08 +0300

    8

--- src/components/ContactList/Contact/Contact.jsx
+++ src/components/ContactList/Contact/Contact.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { fetchDeleteContact } from 'redux/contactThunk';
-import { selectIsLoading } from 'redux/selectors';
 
 import css from './contact.module.css';
 
